cd39d159179b69ca9856871fd7f7bd762b9c853d
benim <martin.artan.andersson@gmail.com>
2023-07-01 16:18:27 +0200

    Added logo in header and reordered header bar

--- cocktails/src/components/TopBar.tsx
+++ cocktails/src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { AppBar, Toolbar, Typography, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Box } from '@mui/material';
 
 interface TopBarProps {
   categories: string[];
@@ -11,20 +11,30 @@ interface TopBarProps {
 const TopBar: React.FC<TopBarProps> = ({ categories, selectedCategory, handleCategoryChange }) => {
   return (
     <AppBar position="static" style={{ marginBottom: '20px' }}>
-      <Toolbar>
-        <Typography variant="h6" component={Link} to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-        Crafty Mixology
-        </Typography>
-        <FormControl variant="outlined" margin="normal" style={{ marginLeft: '1em', width: '200px' }} >
-          <InputLabel id="category-label">Category</InputLabel>
-          <Select labelId="category-label" value={selectedCategory} onChange={handleCategoryChange} label="Category" >
-            {categories.map((category) => (
-              <MenuItem key={category} value={category} >
-                {category}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <FormControl variant="outlined" margin="normal" style={{ marginRight: '1em', width: '200px' }}>
+            <InputLabel id="category-label">Category</InputLabel>
+            <Select labelId="category-label" value={selectedCategory} onChange={handleCategoryChange} label="Category">
+              {categories.map((category) => (
+                <MenuItem key={category} value={category}>
+                  {category}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="Logo" style={{ height: '30px', marginRight: '10px' }} />
+          <Typography variant="h6" component={Link} to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            Crafty Mixology
+          </Typography>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <TextField id="outlined-basic" label="Search" variant="outlined" />
+        </div>
+
       </Toolbar>
     </AppBar>
   );
